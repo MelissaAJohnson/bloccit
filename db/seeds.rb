5daf8884fd98c21f87a2f420ecdcec4d18d3ba24
melissajstudent <missyj2007@gmail.com>
2016-07-10 21:17:24 -0500

    Assignment 30 Seeding Data complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,28 @@
 require 'random_data'
 
  # Create Posts
- 50.times do
-   Post.create!(
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+50.times do
+  Post.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+    )
+end
+
+Post.find_or_create_by(
+  title: 'My seeded title',
+  body: 'My seeded body'
+  )
+
+posts = Post.all
 
  # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+100.times do
+ Comment.create!(
+   post: posts.sample,
+   body: RandomData.random_paragraph
+ )
+end
+
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
